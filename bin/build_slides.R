e8fa8490058b7542d43ea9f50f462c8c7667e9ec
Ian Carroll <icarroll@sesync.org>
2017-07-08 12:17:21 -0400

    turn on caching for Rmd

--- bin/build_slides.R
+++ bin/build_slides.R
@@ -3,47 +3,49 @@ require(knitr)
 require(yaml)
 require(stringr)
 
-config = yaml.load_file("docs/_config.yml")
-render_markdown(fence_char = "~")
+config = yaml.load_file('docs/_config.yml')
+render_markdown(fence_char = '~')
 opts_knit$set(
     root.dir = '.',
     base.dir = 'docs/',
     base.url = '{{ site.baseurl }}/')
 opts_chunk$set(
     comment = NA,
-    fig.path = "images/",
-    block_ial = c("{:.input}", "{:.output}"))
+    fig.path = 'images/',
+    block_ial = c('{:.input}', '{:.output}'),
+    cache = TRUE,
+    cache.path = 'docs/_slides_Rmd/cache/')
 
-current_chunk = knit_hooks$get("chunk")
+current_chunk = knit_hooks$get('chunk')
 chunk = function(x, options) {
     x <- current_chunk(x, options)
     if (!is.null(options$title)) {
-        x <- gsub("~~~(\n*(!\\[.+)?$)",
-                  paste0("~~~\n{:.text-document title=\"", options$title, "\"}\\1"),
+        x <- gsub('~~~(\n*(!\\[.+)?$)',
+                  paste0('~~~\n{:.text-document title="', options$title, '"}\\1'),
                   x)
         return(x)
     }
-    x <- gsub("~~~\n(\n+~~~)",
-              paste0("~~~\n", options$block_ial[1], "\\1"),
+    x <- gsub('~~~\n(\n+~~~)',
+              paste0('~~~\n', options$block_ial[1], '\\1'),
               x)
-    if (str_count(x, "~~~") > 2) {
+    if (str_count(x, '~~~') > 2) {
         idx <- 2
     } else {
         idx <- 1
     }
-    x <- gsub("~~~(\n*$)",
-              paste0("~~~\n", options$block_ial[idx], "\\1"),
+    x <- gsub('~~~(\n*$)',
+              paste0('~~~\n', options$block_ial[idx], '\\1'),
               x)
     return(x)
 }
 knit_hooks$set(chunk = chunk)
 
-files <- list.files("docs/_slides_Rmd")
+files <- list.files('docs/_slides_Rmd')
 for (f in config$slide_sorter) {
-    f.Rmd <- paste0(f, ".Rmd")
+    f.Rmd <- paste0(f, '.Rmd')
     if (f.Rmd %in% files) {
-        f.md <- paste0(f, ".md")
-        knit(input = file.path("docs/_slides_Rmd", f.Rmd),
-             output = file.path("docs/_slides", f.md))
+        f.md <- paste0(f, '.md')
+        knit(input = file.path('docs/_slides_Rmd', f.Rmd),
+             output = file.path('docs/_slides', f.md))
     }
 }
