f9288149f45722ad4d5e6827b9c8c185a92259e0
Ian Carroll <icarroll@sesync.org>
2018-04-02 22:12:01 -0400

    new tool

---
+++ bin/build_worksheet.R
@@ -0,0 +1,14 @@
+library(rvest)
+
+args <- commandArgs(TRUE)
+url <- args[[1]]
+s <- html_session(url)
+n <- html_nodes(s, css = 'h2, .text-document pre')
+
+ws <- lapply(n, function(x) {
+  out <- html_text(x)
+  if (html_name(x) == 'h2') out <- paste('##', out, '\n')
+  return(out)
+})
+cat(paste(ws, collapse = '\n'), file = 'worksheet.R')
+    
