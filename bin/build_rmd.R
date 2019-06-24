fcb9c6011deac388dc909d482073bc3ffea627f2
itcarroll <icarroll@sesync.org>
2019-06-24 15:51:37 -0400

    python engine compatible

--- bin/build_rmd.R
+++ bin/build_rmd.R
@@ -3,6 +3,7 @@ require(methods)
 require(knitr)
 require(yaml)
 require(stringr)
+require(reticulate)
 
 config <- yaml.load_file('docs/_data/lesson.yml')
 render_markdown(fence_char = '~')
@@ -12,60 +13,62 @@ opts_knit$set(
 opts_chunk$set(
     comment = NA,
     cache = TRUE,
-    prompt = TRUE,
+    prompt = FALSE,
     error = FALSE,
+    title = "Console",
+    text.document = TRUE,
     fig.cap = ' ', # whitespace forces .caption after htmlwidget
     screenshot.force = FALSE)
 
-in_ial <- '\n{:%s title="%s"}' #was .input title="Console"
+in_ial <- '\n{:title="%s"%s}'
 out_ial <- '\n{:.output}'
 fig_ial <- '\n{:.captioned}'
   
-knit_hooks$set(chunk = function(x, options) {
-    if (options$eval) {
-        in_ial <- '\n{:%s title="%s"}'
-    } else {
-        in_ial <- '\n{:%s .no-eval title="%s"}'
-    }
-    if (!is.null(options$handout)) {
-        in_ial <- sprintf(in_ial, '.text-document', '{{ site.data.lesson.handouts[%d] }}')
-        in_ial <- sprintf(in_ial, options$handout - 1)
-    } else if (!is.null(options$title)) {
-        in_ial <- sprintf(in_ial, '.text-document', options$title)
-    } else {
-        in_ial <- sprintf(in_ial, '.text-document', 'Console')
-    }
-  
-    # add 'input' class or 'text-document' class with 'title' attribute to code
-    x <- gsub('(~~~r\n.+?~~~)(\n|$)', paste0('\\1', in_ial), x)
-    
-    # add 'output' class to code
-    x <- gsub('(?s)(~~~\n(?!{:).+?~~~)(\n|$)', paste0('\\1', out_ial), x, perl = TRUE)
-
-    # correct figure paths and add 'captioned' class
-    x <- gsub('(!\\[.+?)\\((.*?)\\)(\n|$)', paste0(
-              '\\1({{ "\\2" | prepend: site.imageurl | relative_url }})', fig_ial), x)
-    
-    return(x)
-})
 opts_hooks$set(title = function(options) {
-  options$prompt <- FALSE
+  if (!is.null(options$handout)) {
+    options$title <- sprintf('{{ site.data.lesson.handouts[%d] }}', options$handout - 1)
+  } else if (options$title == "Console") {
+    options$prompt <- TRUE
+    options$text.document <- FALSE
+  }
   options
 })
+knit_hooks$set(chunk = function(x, options) {
+  if (!options$eval) {
+    in_ial <- sprintf(in_ial, '%s', ' .no-eval%s')
+  }
+  if (options$text.document) {
+    in_ial <- sprintf(in_ial, '%s', ' .text-document%s')
+  } else {
+    in_ial <- sprintf(in_ial, '%s', ' .input%s')
+  }
+  in_ial <- sprintf(in_ial, options$title, '')
+  
+  # add 'input' class or 'text-document' class with 'title' attribute to code
+  in_block <- sprintf('(~~~%s\n.+?~~~)(\n|$)', tolower(options$engine))
+  x <- gsub(in_block, paste0('\\1', in_ial), x)
+  
+  # add 'output' class to code
+  x <- gsub('(?s)(~~~\n(?!{:).+?~~~)(\n|$)', paste0('\\1', out_ial), x, perl = TRUE)
+  
+  # correct figure paths and add 'captioned' class
+  x <- gsub('(!\\[.+?)\\((.*?)\\)(\n|$)', paste0(
+    '\\1({{ "\\2" | prepend: site.imageurl | relative_url }})', fig_ial), x)
+  
+  return(x)
+})
 
-files <- list.files('slides')
 deps <- list()
 for (f in config$sorter) {
-    f.Rmd <- paste0(f, '.Rmd')
-    if (f.Rmd %in% files) {
-        f.md <- paste0(f, '.md')
-        opts_chunk$set(
-          fig.path = file.path(f, ''),
-          cache.path = file.path('cache', f, ''))
-        knit(input = file.path('slides', f.Rmd),
-             output = file.path('docs', '_slides', f.md))
-        deps <- c(deps, knit_meta())
-    }
+  f.Rmd <- paste0(f, '.Rmd')
+  f.md <- paste0(f, '.md')
+  opts_chunk$set(
+    fig.path = file.path(f, ''),
+    cache.path = file.path('cache', f, ''))
+  knit(
+    input = file.path('slides', f.Rmd),
+    output = file.path('docs', '_slides', f.md))
+  deps <- c(deps, knit_meta())
 }
 deps <- unique(deps)
 deps <- lapply(deps, FUN = function(d) {
